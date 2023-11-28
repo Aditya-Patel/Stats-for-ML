e5b15dcb590e24c38705875d2ab6df5e8f7591b3
Aditya Patel <aditya.patel1616@gmail.com>
2023-11-28 14:33:49 -0600

    Completion of neural network classification

--- HW 4/MNISTImplementation.py
+++ HW 4/MNISTImplementation.py
@@ -1,48 +1,89 @@
 """
 Aditya Patel
-Implementation of the 10-Classification model on 
+Implementation of the 10-Classification model
 Stat 598 - Statistical Machine Learning 
 """
 
 import numpy as np
-import torch
-from torch import nn
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
-class MNISTModel(nn.Module):
-    def __init__(self, input_features, output_features, hidden_units=8):
-        super.__init__()
-        
-        # Define NN layer stack - 3 features
-        self.layer_stack = nn.Sequential(
-            nn.Linear(input_features, hidden_units),
-            nn.Linear(hidden_units, hidden_units),
-            nn.Linear(hidden_units, output_features)
-        )
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
 
-        def pass_forward(self, x):
-            return self.layer_stack(x)
+EPOCH_COUNT = 50
+BATCH_SIZE = 86
 
-def MNISTDriver(Xtrain: np.ndarray, yTrain: np.ndarray, XTest:np.ndarray, yTest:np.ndarray, out_features=1, hidden_layers=8, neurons=10, optimizer='sgd'):
-    # Check shapes of labels and targets
-    print("Shape of training dataset:\n\tX:{}\n\tY:{}".format(Xtrain.shape, yTrain.shape[1]))
-    print("Shape of test dataset:\n\tX:{}\n\tY:{}".format(XTest.shape, yTest.shape))
+MNIST_TRAIN_DIR = os.getcwd() + '/mnist_train.csv'
+MNIST_TEST_DIR = os.getcwd() + '/mnist_test.csv'
 
-    # Convert to pyTorch Tensors
-    X_train_tensor = torch.from_numpy(Xtrain)
-    X_test_tensor = torch.from_numpy(XTest)
-    y_train_tensor = torch.from_numpy(yTrain)
-    y_test_tensor = torch.from_numpy(yTest)
+def ImportMNISTDataFromDirectory(train_dir, test_dir):
+    mnist_test = pd.read_csv(test_dir)
+    mnist_train = pd.read_csv(train_dir)
 
-    in_features = Xtrain.shape[1]
-    try:
-        out_features = yTrain.shape[1]
-    except IndexError:
-        out_features = 1
-    hidden_units = 8
+    Xt_df = mnist_test.drop(['label'], axis=1)
+    yt_df = mnist_test.label
+    Xr_df = mnist_train.drop(['label'], axis=1)
+    yr_df = mnist_train.label
+    return Xt_df, yt_df, Xr_df, yr_df
 
-    # Device agnostic code
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def CreateTensorflowDatasets(Xt, yt, Xr, yr):
+    X_r = Xr.values.reshape(-1, 28, 28, 1)
+    X_t = Xt.values.reshape(-1, 28, 28, 1)
+    y_r = tf.keras.utils.to_categorical(yr, num_classes=10)
+    y_t = tf.keras.utils.to_categorical(yt, num_classes=10)    
 
-    # Define model
-    mnist_mod = MNISTModel(in_features, out_features, hidden_units).to(device)
-    
+    rescale_data = keras.Sequential([keras.layers.Rescaling(1/255.)])
+    X_r, X_v, y_r, y_v = train_test_split(X_r, y_r, test_size=0.2, random_state=42)
+
+    train_ds = tf.data.Dataset.from_tensor_slices((X_r, y_r))
+    vald_ds = tf.data.Dataset.from_tensor_slices((X_v, y_v))
+    test_ds = tf.data.Dataset.from_tensor_slices((X_t, y_t))
+
+    train_ds = train_ds.map(lambda x, y: (rescale_data(x), y)).shuffle(1000).batch(BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
+    vald_ds = vald_ds.map(lambda x, y: (rescale_data(x), y)).shuffle(1000).batch(BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
+    test_ds = test_ds.map(lambda x, y: (rescale_data(x), y)).shuffle(1000).batch(BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE) 
+
+    return train_ds, vald_ds, test_ds
+
+if __name__ == '__main__':
+    # Create and preprocess test, train, and validationd datasets
+    Xt_df, yt_df, Xr_df, yr_df = ImportMNISTDataFromDirectory(MNIST_TRAIN_DIR, MNIST_TEST_DIR)
+    train, vald, test = CreateTensorflowDatasets(Xt_df, yt_df, Xr_df, yr_df)
+
+    # Define, compile, and fit NN model
+    model = keras.models.Sequential([
+        # Image Convolution and downsampling - Node 1
+        keras.layers.Conv2D(input_shape=(28,28,1), filters=64, kernel_size=3, padding='same', activation='relu'),
+        keras.layers.MaxPool2D(pool_size=2, padding='same'),
+        # Image Convolution and downsampling - Node 2
+        keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+        keras.layers.MaxPool2D(pool_size=2, padding='same'),
+        # Image Convolution and downsampling - Node 3
+        keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+        keras.layers.MaxPool2D(pool_size=2, padding='same'),
+        # Reduce to 1-D vector
+        keras.layers.Flatten(),
+        # Categorization using DNNs 
+        keras.layers.Dense(256, activation='relu'),
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    lr_reducer = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.1, patience=2, min_lr=0.0000001)    
+    hist = model.fit(train, validation_data = vald, epochs = EPOCH_COUNT, callbacks=[lr_reducer], shuffle=True,)
+
+    # Evaluate against test data
+    future = model.evaluate(test, verbose=2)
+
+    # Plot error
+    x = np.array(hist.epoch)
+    y = np.array(hist.history['accuracy'])
+    y = np.multiply(np.subtract(1, y), 100)
+    plt.plot(x, y)
+    plt.title(f'Test Error in Percent vs. Epoch\nModel Prediction Error = {(1-future[1])*100:.2f}%')
+    plt.xlabel('Epoch')
+    plt.ylabel('Test Error (%)')
+    plt.show()
